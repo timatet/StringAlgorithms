cd5eefb4b4fb78c4acdd26f4ba8d2dd4c8f0e8ab
Timofei Teterin <timofei.teterin@inbox.ru>
2022-04-13 14:06:12 +0300

    Добавьте файлы проекта.

---
+++ StringAlgorithms/ISubstringSearch.cs
@@ -0,0 +1,10 @@
+﻿namespace StringAlgorithms
+{
+    public interface ISubstringSearch
+    {
+        /// <summary>
+        /// Возвращает список позиций, начиная с которых строка pattern входит в текст text.
+        /// </summary>
+        string IndexesOf(string pattern, string text);
+    }
+}
---
+++ StringAlgorithmsUnitTest/AlgmUnitTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace StringAlgorithmsUnitTest
+{
+    public class AlgmUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
