2659adab3570c553a40b443d3157339592091e3f
Timofei Teterin <timofei.teterin@inbox.ru>
2022-04-13 22:18:30 +0300

    testing modified

--- StringAlgorithmsUnitTest/AlgmUnitTest.cs
+++ StringAlgorithmsUnitTest/AlgmUnitTest.cs
@@ -1,28 +1,28 @@
 using StringAlgorithms;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace StringAlgorithmsUnitTest
 {
     public class AlgmUnitTest
     {
-        [Fact]
-        public void SearchTest()
+        [Theory]
+        [InlineData("", "", new int[] { 0 })]
+        [InlineData("a", "aaa", new int[] { 0, 1, 2 })]
+        [InlineData("aa", "aaaaaa", new int[] { 0, 1, 2, 3, 4 })]
+        [InlineData("aa", "aabaaa", new int[] { 0, 3, 4 })]
+        [InlineData("abc", "abcabcabc", new int[] { 0, 3, 6 })]
+        public void SearchTest(string pattern, string text, IEnumerable<int> expectedPositions)
         {
             var algms = new List<ISubstringSearch>()
             {
                 new BruteForceAlgorithm(),
             };
 
-            string text = "aaaaabaaaaa"; //10
-            string pattern = "aa";
-            var expected = Enumerable.Range(0, 10).ToList();
-            expected.RemoveRange(4, 2);
             foreach (var algm in algms)
             {
                 var actual = algm.IndexesOf(pattern, text);
-                Assert.Equal(expected, actual);
+                Assert.Equal(expectedPositions, actual);
             }
         }
     }
